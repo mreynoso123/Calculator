22384c58c55c47c7e0602d3bc70e36868173b6eb
Mauricio Reynoso <maureynoso@csumb.edu>
2026-02-11 18:21:22 -0800

    Very basic operations of add, subtract, multiply, and divide

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! My first calculator program");
+        System.out.println("============================================");
+        System.out.println("[1] Addition");
+        System.out.println("[2] Subtraction");
+        System.out.println("[3] Multiplication");
+        System.out.println("[4] Division");
+        System.out.print("What operation are you looking to do? ");
+        int operation = scanner.nextInt();
+
+        System.out.print("Enter first number: ");
+        int input1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int input2 = scanner.nextInt();
+        int total = 0;
+
+        if (operation == 1) {
+            total = input1 + input2;
+        } else if (operation == 2) {
+            total = input1 - input2;
+        } else if (operation == 3) {
+            total = input1 * input2;
+        } else {
+            total = input1 / input2;
+        }
+
+        System.out.println("The output is: " + total);
+
+        scanner.close();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Calculator
 Just a personal calculator project
+
+2/11/2016
+-----------------
+I started v1 just on a whim for no particular reason.
